9ab031faa0932daa6e69da89f2d7c5e5815ec97c
Aravind590 <aravindmulukuri@gmail.com>
2025-12-26 20:42:50 +0530

    Update fetch_review.py

--- fetch_review.py
+++ fetch_review.py
@@ -1,9 +1,7 @@
-
 from google_play_scraper import reviews, Sort
 from datetime import datetime, timedelta
 from collections import defaultdict
 import pandas as pd
-
 
 def fetch_reviews_for_date(app_id, date_str):
     target_date = datetime.strptime(date_str, "%Y-%m-%d").date()
